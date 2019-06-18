a4a38f5e66b6f98d16275d0101cb706fad4bcae4
pipicaca123 <u9593103@gmail.com>
2019-06-18 19:10:28 +0800

    000

--- 2_svc_num/main.c
+++ 2_svc_num/main.c
@@ -50,42 +50,51 @@ void user_task(void)
 	blink(LED_BLUE); //should not return
 }
 
-void svc_handler_c(??????, ??????)
+void svc_handler_c(uint32_t lr_value,uint32_t MSP_value)
 {
-	printf("[SVC Handler] LR: 0x%X\r\n", (unsigned int)??????);
-	printf("[SVC Handler] MSP Backup: 0x%X \r\n", (unsigned int)??????);
+	printf("[SVC Handler] LR: 0x%X\r\n", (unsigned int)lr_value);
+	printf("[SVC Handler] MSP Backup: 0x%X \r\n", (unsigned int)MSP_value);
 	printf("[SVC Handler] Control: 0x%X\r\n", (unsigned int)read_ctrl());
 	printf("[SVC Handler] SP: 0x%X \r\n", (unsigned int)read_sp());
 	printf("[SVC Handler] MSP: 0x%X \r\n", (unsigned int)read_msp());
 	printf("[SVC Handler] PSP: 0x%X \r\n\n", (unsigned int)read_psp());
 
 	uint32_t *stack_frame_ptr;
-	if (??????) //Test bit 2 of EXC_RETURN
+	if ((0x4) & lr_value) //Test bit 2 of EXC_RETURN
 	{
-		stack_frame_ptr = ?????? //if 1, stacking used PSP
+		(uint32_t *)read_psp();
+		stack_frame_ptr =read_psp();  //if 1, stacking used PSP
+		printf("*******flag PSP*******\n\r");
 		printf("[SVC Handler] Stacking used PSP: 0x%X \r\n\n", (unsigned int)stack_frame_ptr);
 	}
 	else
 	{
-		stack_frame_ptr = ?????? //if 0, stacking used MSP
+		(uint32_t *)MSP_value ;
+		stack_frame_ptr =MSP_value ;//if 0, stacking used MSP
+		printf("********flag 2 MSP******\n");
 		printf("[SVC Handler] Stacking used MSP: 0x%X \r\n\n", (unsigned int)stack_frame_ptr);
 	}
 
-	uint32_t stacked_r0 = ??????
-	uint32_t stacked_r1 = ??????
-	uint32_t stacked_return_addr = ??????
+	uint32_t stacked_r0 = stack_frame_ptr[0];
+	uint32_t stacked_r1 = stack_frame_ptr[1];
+	uint32_t stacked_return_addr = stack_frame_ptr[6];
 
-	uint16_t svc_instruction = ??????
-	uint8_t svc_num = ??????
+	uint16_t svc_instruction = *((uint16_t *)stacked_return_addr - 1);
+	uint8_t svc_num = svc_instruction;
 
 	printf("[SVC Handler] Stacked R0: 0x%X \r\n", (unsigned int)stacked_r0);
 	printf("[SVC Handler] Stacked R1: 0x%X \r\n", (unsigned int)stacked_r1);
 	printf("[SVC Handler] SVC number: 0x%X \r\n\n", (unsigned int)svc_num);
 
-	if (svc_num == 0xA)
+	if (svc_num == 0xA){
 		//return r0 + r1
-		??????
-	else
+		printf("0xA of SVC");
+		stack_frame_ptr[0]=stack_frame_ptr[0]+stack_frame_ptr[1];
+		 
+	}
+	else{
+		printf("not 0xA of SVC");
 		//return 0
-		??????
+		stack_frame_ptr[0]=0;
+	}
 }
